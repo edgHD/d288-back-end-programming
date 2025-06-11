2f29dc8a8cdf055e338431634a7af7fe39e4319e
Admin <edolz1@wgu.edu>
2025-06-10 20:02:42 -0400

    D. Added Entity classes

---
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -0,0 +1,53 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "customer_first_name")
+    private String firstName;
+
+    @Column(name = "customer_last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postal_code")
+    private String postal_code;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "division_id")
+    private Division division;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Cart> carts = new HashSet<>();
+}
---
+++ src/main/java/com/example/demo/entities/Division.java
@@ -0,0 +1,41 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "divisions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Division {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "division_id")
+    private Long id;
+
+    @Column(name = "division")
+    private String division_name;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @OneToMany(mappedBy = "division")
+    private Set<Customer> customers = new HashSet<>();
+}
